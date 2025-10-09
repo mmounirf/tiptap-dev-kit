26e0ddf4ba9a532baf247240cd54e5386703532f
Mou <m.mounir.f@gmail.com>
2025-10-09 11:05:23 +0200

    feat: update markdown demo example

--- src/assets/markdown-demo.ts
+++ src/assets/markdown-demo.ts
@@ -1,23 +1,13 @@
-export const demoMarkdown = `
-hi there is is markdown demo
-`;
-
 export const demoMarkdownMentions = `
-**The current mentions uses dummy data, for files we have static list, for users, we hit a json placeholder api**
+User mentions from [json placeholder api](https://jsonplaceholder.typicode.com/)
 
-Now I can mention users like: <@U1> which will resolve to our custom syntax \`<@U1>\`.
+- <@U1>
+- <@U2>
+- <@U3>
 
-> 1 is the user id from users api being used in the demo
+And file mentions just use hardcoded static data
 
-I'm extensible enough to support any kind of custom syntax, as an example, imagine a file entity mention custom syntax.
-
-> Following our previous practice with users mention, I imagine it will look like this
-
-\`\`\`markdown
-<@F123-456-789>
-
-// Where "F" indicates a file mention, right?
-\`\`\`
-
-File entity also have its own custom node, as an example this is how files are being mentioned in this demo #WEBP - node renderer is basically a react component - so it's your playground to craft interactive rich nodes
-`;
+- [x] <@FPDF>
+- [x] <@FWEBP>
+- [x] <@FPNG>
+`
